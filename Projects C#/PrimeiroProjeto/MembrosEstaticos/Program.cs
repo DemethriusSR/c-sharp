76f73c878f90e35b726afdd43f3579fc4a612ecb
Demethrius Rodrigues <demethriussr@gmail.com>
2024-02-14 17:52:12 -0400

    New Archive MembrosEstaticos

---
+++ Projects C#/PrimeiroProjeto/MembrosEstaticos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+/* Teste 1 */
+namespace MembrosEstaticos
+{
+    class Program
+    {
+        static double Pi = 3.14;
+        static void Main(string[] args)
+        {
+
+            Console.Write("Entre com RAIO: ");
+            double raio = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            double circ = Circunferencia(raio);
+
+            Console.Write($" Circunferencia = {circ:F2}");
+        }
+
+        static double Circunferencia (double raio)
+        {
+            return 2.0 * Pi * raio;
+        }
+    }
+}
