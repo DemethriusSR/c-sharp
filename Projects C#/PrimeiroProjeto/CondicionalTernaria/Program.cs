449802681306c76974b1f918133126f21324d5dd
Demethrius Rodrigues <demethriussr@gmail.com>
2024-04-16 21:54:18 -0400

    Add Condicional ternaria file

---
+++ Projects C#/PrimeiroProjeto/CondicionalTernaria/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+/* Condicional Ternária */
+namespace CondicionalTernaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Valor: ");
+            double num = int.Parse(Console.ReadLine());
+
+            double desconto = (num < 20.0) ? num * 0.1 : num * 0.05; // Condicao Ternaria
+
+            Console.WriteLine($"Desconto = R$ {desconto.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
