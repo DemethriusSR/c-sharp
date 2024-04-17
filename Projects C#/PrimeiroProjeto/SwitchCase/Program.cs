c1478654156c4424b33c36a856fd7bf3d6ead8e8
Demethrius Rodrigues <demethriussr@gmail.com>
2024-04-16 21:30:04 -0400

    Add Switch case file

---
+++ Projects C#/PrimeiroProjeto/SwitchCase/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+/* Switch case */
+namespace SwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Dia: ");
+            int x = int.Parse(Console.ReadLine());
+            string day;
+
+            switch(x){
+                case 1:
+                    day = "Sunday";
+                break;
+                case 2:
+                    day = "Monday";
+                break;
+                case 3:
+                    day = "Tuesday";
+                break;
+                case 4:
+                    day = "Wednesday";
+                break;
+                case 5:
+                    day = "Thursday";
+                break;
+                case 6:
+                    day = "Friday";
+                break;
+                case 7:
+                    day = "Saturday";
+                break;
+                default:
+                    day = "Invalid Day";
+                break;
+            }
+            Console.WriteLine($"Your Choice is {day}");
+
+        }
+    }
+}
