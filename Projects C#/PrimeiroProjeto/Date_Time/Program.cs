3afea8a40b3959b2c4b69a1b00919166545a0034
Demethrius Rodrigues <demethriussr@gmail.com>
2024-04-18 18:27:18 -0400

    Add file Date_Time

---
+++ Projects C#/PrimeiroProjeto/Date_Time/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+/* Teste 02 */
+namespace Date_Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d1 = new DateTime(2018, 11, 25);
+            DateTime d2 = new DateTime(2018, 11, 25, 20, 45, 3);
+            DateTime d3 = new DateTime(2018, 11, 25, 20, 45, 3, 500);
+
+            DateTime d4 = DateTime.Now;
+            DateTime d5 = DateTime.Today;
+            DateTime d6 = DateTime.UtcNow;
+
+            DateTime d7 = DateTime.Parse("2000-08-15");
+            DateTime d8 = DateTime.Parse("2000-08-15 13:05:58");
+
+            DateTime d9 = DateTime.ParseExact("2000-08-15", "yyyy-MM-dd", CultureInfo.InvariantCulture);
+            DateTime d10 = DateTime.ParseExact("2000-08-15 13:05:58", "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
+
+
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+            Console.WriteLine(d3);
+            Console.WriteLine(d4);
+            Console.WriteLine(d5);
+            Console.WriteLine(d6);
+            Console.WriteLine(d7);
+            Console.WriteLine(d8);
+            Console.WriteLine(d9);
+            Console.WriteLine(d10);
+        }
+    }
+}
